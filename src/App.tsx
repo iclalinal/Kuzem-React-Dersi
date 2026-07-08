dcc9c0a4dd9b99c436a223102c9014c1ff82f48b
iclalinal <211450002@ogrenci.karatay.edu.tr>
2026-07-08 20:57:07 +0300

    Daha fazla örnek eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ function App() {
   return(
     <div>
       <Kart baslik = "Kalem" fiyat ={5} aciklama = "Bu bir kalemdir." />
-      <Kart baslik = "Klavye" fiyat ={500} />
+      <Kart baslik = "Klavye" fiyat ={500} aciklama = "Bu bir klavyedir." />
+      <Kart baslik = "Monitör" fiyat ={500} aciklama = "Bu bir monitördür." />
+      <Kart baslik = "Bilgisayar" fiyat ={500} aciklama = "Bu bir bilgisayardır." />
     </div>
   )
 }
