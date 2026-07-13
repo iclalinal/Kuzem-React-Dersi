ad0a05db124b0af3fdb5220e02eba730a275e0f6
iclalinal <211450002@ogrenci.karatay.edu.tr>
2026-07-13 21:07:12 +0300

    Drilling örneği

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,21 @@
-import Button from "./components/Button";
-import Item from "./components/Item";
-import Card from "./components/Card";
+interface User{
+  name: string;
+}
 
 function App() {
-  const count = 0;
-  function handleClick(){
-    console.log("Butona tıklandı");
-  }
-
-  function handleIncrement(){
-    console.log("sayac arttırıldı", count + 1);
-  }
-
-  return(
-    <>
-    <Card>
-      <h2>İclal İnal</h2>
-      <p>React öğreniyorum</p>
-    </Card>
-    <p> sayac: {count}</p>
-    <Button label = "sayac" onClick={handleIncrement} />
-    <Item id={1} name="Kalem" onDelete={(id) => console.log("Silindi:", id)} />
-    <Button label="Tıkla" onClick={handleClick} />
-    <Card>
-      <h2>Diğer Kart</h2>
-      <p>Bu bir başka karttır</p>
-    </Card>
-    </>
-  )
+  const user: User = {name:"İclal İnal"};
+  return <Page user={user} />
 }
+
+function Page({user}:{user:User}){
+  return <Headers user={user} />
+}
+
+function Headers({user}:{user:User}){
+  return(
+    <div>
+      <h1>Merhaba {user.name}</h1>
+    </div>
+      )}
+
 export default App;
