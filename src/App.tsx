58e0ad918c6a7f18864cea75d4d078fc40dc723d
iclalinal <211450002@ogrenci.karatay.edu.tr>
2026-07-13 20:42:36 +0300

    sayac eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import Button from "./components/Button";
 import Item from "./components/Item";
 
 function App() {
+  const count = 0;
   function handleClick(){
     console.log("Butona tıklandı");
   }
 
+  function handleIncrement(){
+    console.log("sayac arttırıldı", count + 1);
+  }
 
   return(
     <>
+    <p> sayac: {count}</p>
+    <Button label = "sayac" onClick={handleIncrement} />
     <Item id={1} name="Kalem" onDelete={(id) => console.log("Silindi:", id)} />
     <Button label="Tıkla" onClick={handleClick} />
     </>
