458533d3e137c43b200accba7f47b104de83eb4b
iclalinal <211450002@ogrenci.karatay.edu.tr>
2026-07-20 21:04:33 +0300

    arttırma azaltma sıfırlama butonu eklendi

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,11 +2,13 @@ import {useState} from 'react';
 
 function Counter(){
     const [count, setCount] = useState(0);
-    console.log("render count", count);
+
     return(
         <div>
             <p> deger : {count} </p>
-            <button onClick={()=>setCount(count+1)}>Arttır</button>
+            <button onClick={()=>setCount(p => p+1)}>Arttır</button>
+            <button disabled={count === 0} onClick={()=>setCount(p => Math.max(0, p-1))}>Azalt</button>
+            <button onClick={()=>setCount(0)}>Sıfırla</button>
         </div>
     )
 }
