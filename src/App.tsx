d6b17b9e357fe2377dcbe1a80344ae09092f8e4a
iclalinal <211450002@ogrenci.karatay.edu.tr>
2026-08-26 21:02:05 +0300

    ToDo Route eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router-dom";
 import KullaniciRehberi from "./components/KullaniciRehberi";
 import Layout from "./components/Layout";
 import Spinner from './components/Spinner';
+import ToDoApp from './components/ToDoApp';
 
 function AnaSayfa() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route index element={<AnaSayfa />} />
         <Route path="kullanicirehberi" element={<KullaniciRehberi />} />
         <Route path="hakkimizda" element={<h1>Hakkımızda</h1>} />
+        <Route path ="todos" element={<ToDoApp />} />
         <Route path="spinner" element={<Spinner />} />
       </Route>
     </Routes>
